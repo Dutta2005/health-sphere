cc2e57ec3afa912f39349265dfd4c944ec39c6af
Raj Dutta <duttabipul927@gmail.com>
2025-01-26 08:09:39 +0530

    refactor: clean up post model and routes by removing commented code

--- server/src/models/post.model.js
+++ server/src/models/post.model.js
@@ -1,30 +1,3 @@
-// import mongoose from 'mongoose';
-
-// const postSchema = new mongoose.Schema({
-//     title: {
-//         type: String,
-//         required: true,
-//         trim: true
-//     },
-//     content: {
-//         type: String,
-//         required: true
-//     },
-//     tags: {
-//         type: [String],
-//         default: []
-//     },
-//     author: {
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: 'User',
-//         required: true
-//     }
-// }, {
-//     timestamps: true
-// });
-
-// export const Post = mongoose.model('Post', postSchema);
-
 import mongoose from 'mongoose';
 
 const postSchema = new mongoose.Schema({
@@ -43,16 +16,43 @@ const postSchema = new mongoose.Schema({
     },
     author: {
         type: mongoose.Schema.Types.ObjectId,
-        refPath: 'authorModel',
+        ref: 'User',
         required: true
-    },
-    authorModel: {
-        type: String,
-        required: true,
-        enum: ['User', 'Organization']
     }
 }, {
     timestamps: true
 });
 
 export const Post = mongoose.model('Post', postSchema);
+
+// import mongoose from 'mongoose';
+
+// const postSchema = new mongoose.Schema({
+//     title: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     content: {
+//         type: String,
+//         required: true
+//     },
+//     tags: {
+//         type: [String],
+//         default: []
+//     },
+//     author: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         refPath: 'authorModel',
+//         required: true
+//     },
+//     authorModel: {
+//         type: String,
+//         required: true,
+//         enum: ['User', 'Organization']
+//     }
+// }, {
+//     timestamps: true
+// });
+
+// export const Post = mongoose.model('Post', postSchema);
--- server/src/routes/post.routes.js
+++ server/src/routes/post.routes.js
@@ -1,68 +1,68 @@
-// import { Router } from "express";
-// import { 
-//     createPost, 
-//     getAllPosts, 
-//     getPostById, 
-//     getPostsByUserId,
-//     editPost, 
-//     deletePost
-// } from "../controllers/post.controller.js";
-// import { verifyJWT } from "../middlewares/auth.middleware.js";
-
-// const router = Router();
-
-// router.use(verifyJWT);
-
-// // /api/v1/posts
-// router.post("/create", createPost);
-// router.get("/", getAllPosts);
-// router.get("/:id", getPostById);
-// router.get("/user/:id", getPostsByUserId);
-// router.patch("/edit/:id", editPost);
-// router.delete("/delete/:id", deletePost);
-
-// export default router;
-
 import { Router } from "express";
 import { 
     createPost, 
     getAllPosts, 
     getPostById, 
-    getMyPosts,
     getPostsByUserId,
     editPost, 
     deletePost
 } from "../controllers/post.controller.js";
-import { verifyJWT as verifyUserJWT } from "../middlewares/auth.middleware.js";
-import { verifyJWT as verifyOrgJWT } from "../middlewares/org.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-// Middleware to handle both user and organization authentication
-const authenticateEntity = (req, res, next) => {
-    verifyUserJWT(req, res, (userErr) => {
-        if (!userErr) {
-            req.user = req.user;
-            return next();
-        }
-        verifyOrgJWT(req, res, (orgErr) => {
-            if (!orgErr) {
-                req.organization = req.organization;
-                return next();
-            }
-            // If both authentications fail, pass the user error
-            next(userErr || orgErr);
-        });
-    });
-};
+router.use(verifyJWT);
 
-// All routes now protected
-router.post("/create", authenticateEntity, createPost);
-router.get("/", authenticateEntity, getAllPosts);
-router.get("/my-posts", authenticateEntity, getMyPosts);
-router.get("/:id", authenticateEntity, getPostById);
-router.get("/user/:id", authenticateEntity, getPostsByUserId);
-router.patch("/edit/:id", authenticateEntity, editPost);
-router.delete("/delete/:id", authenticateEntity, deletePost);
+// /api/v1/posts
+router.post("/create", createPost);
+router.get("/", getAllPosts);
+router.get("/:id", getPostById);
+router.get("/user/:id", getPostsByUserId);
+router.patch("/edit/:id", editPost);
+router.delete("/delete/:id", deletePost);
 
 export default router;
+
+// import { Router } from "express";
+// import { 
+//     createPost, 
+//     getAllPosts, 
+//     getPostById, 
+//     getMyPosts,
+//     getPostsByUserId,
+//     editPost, 
+//     deletePost
+// } from "../controllers/post.controller.js";
+// import { verifyJWT as verifyUserJWT } from "../middlewares/auth.middleware.js";
+// import { verifyJWT as verifyOrgJWT } from "../middlewares/org.middleware.js";
+
+// const router = Router();
+
+// // Middleware to handle both user and organization authentication
+// const authenticateEntity = (req, res, next) => {
+//     verifyUserJWT(req, res, (userErr) => {
+//         if (!userErr) {
+//             req.user = req.user;
+//             return next();
+//         }
+//         verifyOrgJWT(req, res, (orgErr) => {
+//             if (!orgErr) {
+//                 req.organization = req.organization;
+//                 return next();
+//             }
+//             // If both authentications fail, pass the user error
+//             next(userErr || orgErr);
+//         });
+//     });
+// };
+
+// // All routes now protected
+// router.post("/create", authenticateEntity, createPost);
+// router.get("/", authenticateEntity, getAllPosts);
+// router.get("/my-posts", authenticateEntity, getMyPosts);
+// router.get("/:id", authenticateEntity, getPostById);
+// router.get("/user/:id", authenticateEntity, getPostsByUserId);
+// router.patch("/edit/:id", authenticateEntity, editPost);
+// router.delete("/delete/:id", authenticateEntity, deletePost);
+
+// export default router;
